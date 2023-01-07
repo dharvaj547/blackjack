6b025798280ff58ea733da080dc0382aeaeae1b8
djan862 <djan862@aucklanduni.ac.nz>
2023-01-07 19:46:29 +1300

    feat: add constructor and getValue() functionality

--- Game/Card.cpp
+++ Game/Card.cpp
@@ -1,21 +1,26 @@
 #include "Card.hpp"
 
-Card::Card(Suit s, Rank r) 
-{
-
-}
+Card::Card(Suit s, Rank r) : suit(s), rank(r) {}
 
 Suit Card::getSuit() 
 {
-
+    return suit;
 }
 
 Rank Card::getRank()
 {
-
+    return rank;
 }
 
 int Card::getValue() const 
 {
-    
+    // face cards are worth 10 points
+    if (rank >= JACK)
+        return 10;
+    // aces are worth 1 or 11 points
+    else if (rank == ACE)
+        return 11;
+    // all other cards are worth their face value
+    else
+        return static_cast<int>(rank) + 2;
 }
