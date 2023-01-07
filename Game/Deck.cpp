0ef74e78a12782455730c4dbf60ac70c2072072c
djan862 <djan862@aucklanduni.ac.nz>
2023-01-07 19:48:51 +1300

    feat: add functionality for all methods

--- Game/Deck.cpp
+++ Game/Deck.cpp
@@ -1,16 +1,36 @@
+#include <algorithm>
+#include <random>
 #include "Deck.hpp"
 
 Deck::Deck() 
 {
+    // TODO:MAKE THIS POSSIBLE FOR MULTIPLE DECKS
 
+    // initialize the deck with one of each card
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 13; j++)
+        {
+            cards.push_back(Card(static_cast<Suit>(i), static_cast<Rank>(j)));
+        }
+    }
 }
 
 void Deck::shuffle()
 {
-
+    std::shuffle(this->cards.begin(), this->cards.end(), std::random_device());
 }
 
 Card Deck::dealCard()
 {
-    
+    /* get a better way of doing this!! */
+    if (this->cards.size() <= 0)
+    {
+        std::cerr << "Deck size has reached less than or below 0\n";
+    }
+
+    Card dealt = this->cards.back();
+    this->cards.pop_back();
+
+    return dealt;
 }
