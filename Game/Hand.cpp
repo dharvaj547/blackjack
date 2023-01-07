6cece467fa9ec17470131d696dd8a64d53b55b48
djan862 <djan862@aucklanduni.ac.nz>
2023-01-07 19:50:47 +1300

    feat: add functionality for all methods

--- Game/Hand.cpp
+++ Game/Hand.cpp
@@ -1,16 +1,19 @@
 #include "Hand.hpp"
 
-Hand::Hand() 
-{
-
-}
+Hand::Hand() {}
 
 void Hand::addCard(Card c)
 {
-
+    cards.emplace_back(c);
 }
 
 int Hand::getTotal() const
 {
+    int total = 0;
 
+    for (const auto& card : cards) {
+        total += card.getValue();
+    }
+
+    return total;
 }
