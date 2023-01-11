9a05ead00827e64515b52f60b72dc8e0db84390a
djan862 <djan862@aucklanduni.ac.nz>
2023-01-11 23:00:32 +1300

    feat: add method to print enums as strings

--- Game/Card.cpp
+++ Game/Card.cpp
@@ -1,26 +1,30 @@
 #include "Card.hpp"
 
-Card::Card(Suit s, Rank r) : suit(s), rank(r) {}
+Card::Card(Suit s, Rank r) : suit(s), rank(r) { }
 
-Suit Card::getSuit() 
-{
-    return suit;
-}
+Suit Card::getSuit() const { return suit; }
 
-Rank Card::getRank()
-{
-    return rank;
-}
+Rank Card::getRank() const { return rank; }
 
-int Card::getValue() const 
+std::string Card::getSuitString() const { return suit_strings[suit]; }
+
+std::string Card::getRankString() const { return rank_strings[rank]; }
+
+int Card::getValue() const
 {
-    // face cards are worth 10 points
-    if (rank >= JACK)
-        return 10;
     // aces are worth 1 or 11 points
-    else if (rank == ACE)
+    if (rank == ACE)
         return 11;
+    // face cards are worth 10 points
+    else if (rank >= JACK)
+        return 10;
     // all other cards are worth their face value
     else
-        return static_cast<int>(rank) + 2;
+        return static_cast<int>(rank) + 1;
+}
+
+std::ostream &operator<<(std::ostream &os, const Card &c)
+{
+    os << c.getRankString() << " of " << c.getSuitString();
+    return os;
 }
--- Game/Card.hpp
+++ Game/Card.hpp
@@ -1,7 +1,7 @@
 #ifndef CARD_HPP
 #define CARD_HPP
 
-#include <iostream>
+#include <ostream>
 
 enum Suit
 {
@@ -11,34 +11,57 @@ enum Suit
     DIAMONDS
 };
 
+const std::string suit_strings[] = {"Spades", 
+                                    "Hearts", 
+                                    "Diamonds", 
+                                    "Clubs"};
+
 enum Rank
 {
+    ACE,
     TWO,
     THREE,
     FOUR,
     FIVE,
     SIX,
     SEVEN,
-    EIGTH,
+    EIGHT,
     NINE,
     TEN,
     JACK,
     QUEEN,
-    KING,
-    ACE
+    KING
 };
+
+const std::string rank_strings[] = {"A",
+                                    "2",
+                                    "3",
+                                    "4",
+                                    "5",
+                                    "6",
+                                    "7",
+                                    "8",
+                                    "9",
+                                    "10",
+                                    "J",
+                                    "Q",
+                                    "K"};
 
 class Card
 {
+public:
+    Card(Suit s, Rank r);
+    Suit getSuit() const;
+    Rank getRank() const;
+    std::string getSuitString() const;
+    std::string getRankString() const;
+    int getValue() const;
+
 private:
     Suit suit;
     Rank rank;
-
-public:
-    Card(Suit s, Rank r);
-    Suit getSuit();
-    Rank getRank();
-    int getValue() const;
 };
+
+std::ostream &operator<<(std::ostream &os, const Card &c);
 
 #endif /* CARD_HPP */
