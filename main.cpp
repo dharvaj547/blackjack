fd76765b3a53ccf35bbeae95cd825520ba394b81
Dharva Jani <dharva.jani@zozo.com>
2023-01-19 22:29:44 +1300

    refactor: more aesthetic introduction to game

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-// #include "Game/Card.hpp"
-// #include "Game/Deck.hpp"
-// #include "Game/Hand.hpp"
 #include "Game/Blackjack.hpp"
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+    // TODO: add support to exit game loop
+    // TODO: account for aces
+    // TODO: add ability to split
+    
+    std::cout << "NOTE: press ctrl+c to exit anytime\n";
     std::cout << "Please enter your name: ";
     std::string name;
     std::cin >> name;
-    
     std::cout << "------------------------------------------------------------\n";
-    std::cout << " _____   _____  _____  _____  " << std::endl <<
-                 "|A    | |A    ||A    ||A    | " << std::endl <<
-                 "|  ♠  | |  ♦  ||  ♣  ||  ♥  | " << std::endl <<
-                 "|    A| |    A||    A||    A| " << std::endl <<
-                 " ‾‾‾‾‾   ‾‾‾‾‾  ‾‾‾‾‾  ‾‾‾‾‾  " << std::endl;
-
+    std::cout << " _____   _____  _____  _____  " << std::endl
+              << "|A    | |A    ||A    ||A    | " << std::endl
+              << "|  ♠  | |  ♦  ||  ♣  ||  ♥  | " << std::endl
+              << "|    A| |    A||    A||    A| " << std::endl
+              << " ‾‾‾‾‾   ‾‾‾‾‾  ‾‾‾‾‾  ‾‾‾‾‾  " << std::endl;
     std::cout << "Welcome to the table " << name << "!\n\n";
 
     Blackjack game(name);
-    
-    // TODO: add option to quit game when 'q' is pressed
-    while (true) 
+
+    while (true)
     {
+        std::cout << "------------------------------------------------------------\n\n";
         game.play();
     }
 
